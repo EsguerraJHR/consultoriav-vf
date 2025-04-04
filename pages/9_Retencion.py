8f454af029a61de4216adc41cbd79435aaa33281
EsguerraJHR <hcastro@esguerrajhr.com>
2025-04-04 17:50:47 -0500

    Update 9_Retencion.py

--- pages/9_Retencion.py
+++ pages/9_Retencion.py
@@ -25,7 +25,7 @@ st.title("Consultas sobre Retención en la Fuente")
 
 # Descripción de la página
 st.markdown("""
-Contiene 36 sentencias del Consejo de Estado y 343 conceptos de la Dian. Pueden acceder a los documentos aquí [Biblioteca](https://eba-my.sharepoint.com/:f:/g/personal/hcastro_esguerrajhr_com/EgWozji9P89Gi02QG_0ybskBFzI39tnYkn78gfP3PiGWPw?e=JCZUDU).
+Contiene **36 sentencias del Consejo de Estado y 343 conceptos de la Dian**. Pueden acceder a los documentos aquí [Biblioteca](https://eba-my.sharepoint.com/:f:/g/personal/hcastro_esguerrajhr_com/EgWozji9P89Gi02QG_0ybskBFzI39tnYkn78gfP3PiGWPw?e=JCZUDU).
 """)
 
 # Verificar si la colección existe
